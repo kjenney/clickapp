1977aec6451aaeaa7df403bd685c9fcb37009a4f
Ken Jenney <kjenney@gmail.com>
2026-01-03 13:16:24 -0500

    Fix version and workflows

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = project.findProperty("appVersionName")?.toString() ?: "1.0-dev"
     }
 
     buildTypes {
